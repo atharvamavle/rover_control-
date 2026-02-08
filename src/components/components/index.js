4db8b689728f324473a9ff1372f38ecdd304d034
Atharva Santosh Mavale <amavale34@gmail.com>
2026-02-08 14:56:57 +1100

    Fix: Remove duplicate nested components folder

--- src/components/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Navbar } from './Navbar';
-export { Sidebar } from './Sidebar';
-export { VideoCamera } from './VideoCamera';
-export { GamepadControl } from './GamepadControl';
-export { MapView } from './MapView';
